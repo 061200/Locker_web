46ab47db93d894f7db1fec99993e2761c212f854
devohda <fffvvv6@gmail.com>
2020-11-20 05:32:15 +0900

    버튼 누르면 페이지 이동하게 만듦

--- routes/reservation.js
+++ routes/reservation.js
@@ -15,9 +15,6 @@ router.get('/', function(req, res){
     res.render('reservation');
 });
 
-router.get('/:pageid',(req,res)=>{
-    res.render('reservation',{data:req.params.pageid});
-})
 router.get('/seats', function(req, res){ // /seats 에 상태 보내기
     res.send(lockerJSON.seats);
 });
